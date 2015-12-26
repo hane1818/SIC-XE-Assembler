b66db5205935b9753d69ae21b123900a4af76fcd
Hane Liu <hane0131@gmail.com>
2015-12-26 16:59:55 +0800

    add assembler's constructor & prettify code

--- SIC-XE/Assembler.py
+++ SIC-XE/Assembler.py
@@ -1,6 +1,15 @@
-class Assembler:
-    pass
-
 import sys
+import re
 
-sys.modules[__name__]=Assembler
+
+class Assembler:
+    def __init__(self, filename):
+        fin = open(filename, 'r', encoding="utf-8-sig")
+        if fin:
+            self.source = fin.read().split('\n')
+            for i in self.source:
+                if re.match('^\s*$', i):
+                    self.source.remove(i)
+
+
+sys.modules[__name__] = Assembler
