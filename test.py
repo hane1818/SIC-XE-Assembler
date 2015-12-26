f4b2ae09f718575a7a9fc794f23ac29914a30c8c
Hane Liu <hane0131@gmail.com>
2015-12-26 17:29:06 +0800

    test assembler's constructor

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from SIC-XE import Assembler
+from SICXE import Assembler
 import unittest
 
 class TestAssembler(unittest.TestCase):
-    
+    def setUp(self):
+        self.asm = Assembler("SICXE.txt")
+
+    def test_read_source(self):
+        self.assertIsNotNone(self.asm.source, "Can't read file")
+
+
+if __name__=="__main__":
+    unittest.main()
